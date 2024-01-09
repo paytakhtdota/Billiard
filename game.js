1369035a7d247ac14ab92dbc90ca40d107e633cb
Mo Ansari <paytakhtdota@gmail.com>
2024-01-09 11:52:43 -0800

    stick reposition done!

--- game.js
+++ game.js
@@ -59,6 +59,7 @@ class Vector {
 const BALL_ORIGIN = new Vector(25, 25)
 const STICK_ORIGIN = new Vector(970, 11);
 const SHOOT_ORIGIN = new Vector(950, 11);
+const DELTA = 0.01;
 //////////////////////Vector////////////////////////
 //////////////////////mouse Handler////////////////////////
 class ButtonState {
@@ -148,15 +149,23 @@ class Ball {
     constructor(Position) {
         this.position = Position;
         this.velocity = new Vector();
+        this.moving = false;
     }
-    update(dleta) {
-
+    update(delta) {
+this.position.addTo(this.velocity.mult(delta));
+this.velocity = this.velocity.mult(0.98);
     }
     draw() {
-        Canvas.drawImage(sprites.whiteBall, this.position, BALL_ORIGIN,)
-    }
-    shoot(power, rotation) {
+        Canvas.drawImage(sprites.whiteBall, this.position, BALL_ORIGIN,);
+        if(this.velocity.length()<5){
+            this.velocity = new Vector();
+            this.moving = false;
 
+        }
+    }
+    shoot(power, rotation){
+        this.velocity = new Vector(power * Math.cos(rotation), power * Math.sin(rotation));
+        this.moving = true;
     }
 }
 //////////////////////Ball////////////////////////
@@ -168,18 +177,39 @@ class Stick {
         this.origin = STICK_ORIGIN.copy();
         this.power = 0;
         this.onShoot = onShoot;
+        this.shot = false;
     }
     draw() {
         Canvas.drawImage(sprites.stick, this.position, this.origin, this.rotation)
     }
     update() {
         this.updateRotation();
+        if (Mouse.left.down) {
+            this.increasePower();
+        } else if (this.power > 0) {
+            this.shoot();
+        }
     }
+    shoot() {
+        this.onShoot(this.power, this.rotation);
+        this.power = 0;
+        this.origin = SHOOT_ORIGIN.copy();
+        this.shot = true;
+    }
+
     updateRotation() {
         let opposite = Mouse.position.y - this.position.y;
         let adjacent = Mouse.position.x - this.position.x;
 
         this.rotation = Math.atan2(opposite, adjacent)
+    }
+    increasePower() {
+        this.power += 100;
+        this.origin.x += 5;
+    }
+    reposition(Position){
+        this.position = Position.copy();
+        this.origin = STICK_ORIGIN.copy();
     }
 }
 //////////////////////Stick////////////////////////
@@ -190,7 +220,11 @@ class GameWorld {
         this.stick = new Stick(new Vector(413, 413), this.whiteBall.shoot.bind(this.whiteBall));
     }
     update() {
-        this.stick.update()
+        this.stick.update();
+        this.whiteBall.update(DELTA);
+        if(!this.whiteBall.moving && this.stick.shot){
+            this.stick.reposition(this.whiteBall.position)
+        }
     }
     draw() {
         Canvas.drawImage(sprites.background, { x: 0, y: 0 })
